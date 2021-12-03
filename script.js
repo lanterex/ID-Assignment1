a910a84b8f53c7f1baaf93fb7cc9fd1c5cd22f1b
lanterex <s10204044@connect.np.edu.sg>
2021-12-03 21:06:48 +0800

    Added hamburger menu for all pages + cleanup

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ var counter = 1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
     counter++;
-    if(counter > 3){
+    if(counter > 4){
         counter = 1;
     }
 },7500);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active")
+}))
